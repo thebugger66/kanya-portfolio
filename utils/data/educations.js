3aebe6e4b0a04d140e03e51f61df0950362abce1
Harryverse@66 <168077302+thebugger66@users.noreply.github.com>
2026-05-07 18:08:54 +0530

    Update educations.js

--- utils/data/educations.js
+++ utils/data/educations.js
@@ -1,9 +1,9 @@
 export const educations = [
   {
     id: 1,
-    title: "Bachelor of Finance",
+    title: "Bachelor of Fine Arts",
     duration: "2023-2027",
-    institution: "D.Y Patil International Institute, pune ",
+    institution: "D.Y Patil International University, pune ",
  
   },
 
