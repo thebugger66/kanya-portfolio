da498ac40aaff6252258e6cfa59451aceb950c62
Harryverse@66 <168077302+thebugger66@users.noreply.github.com>
2026-05-07 18:03:38 +0530

    Fix typo in hero section introduction

--- app/components/homepage/hero-section/index.jsx
+++ app/components/homepage/hero-section/index.jsx
@@ -201,7 +201,7 @@ function HeroSection() {
             Hello, <br />
             This is{" "}
             <span className=" text-pink-500">{personalData.name}</span>
-            {` , I'm a Aspiring Designer `}
+            {` , I'm a Aspiring  `}
             <span className=" text-[#16f2b3]">{personalData.designation}</span>.
           </h1>
 
